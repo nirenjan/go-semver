c405329a5d4e27386e5d05957ea79463346d93c1
nirenjan <nirenjan@gmail.com>
2019-08-20 10:05:12 -0700

    Add String formatting

---
+++ lib/string.go
@@ -0,0 +1,30 @@
+package lib
+
+import "fmt"
+
+func formatDotSeparated(s []string) string {
+	var out string
+
+	for _, v := range s {
+		out += fmt.Sprintf(".%s", v)
+	}
+
+	// Drop the leading .
+	return out[1:]
+}
+
+func (s SemVer) String() string {
+	var out string
+
+	out = fmt.Sprintf("%d.%d.%d", s.major, s.minor, s.patch)
+
+	if len(s.prerelease) > 0 {
+		out += "-" + formatDotSeparated(s.prerelease)
+	}
+
+	if len(s.build) > 0 {
+		out += "+" + formatDotSeparated(s.build)
+	}
+
+	return out
+}
